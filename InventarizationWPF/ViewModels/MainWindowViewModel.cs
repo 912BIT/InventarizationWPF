ff13390af5ea76c3967d94fa3ca6383da5a737e6
912bit <freaks338@gmail.com>
2021-04-01 20:47:46 +0700

    Вью-модель окна MainWindow

---
+++ InventarizationWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using InventarizationWPF.Infrastructure.Commands;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace InventarizationWPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Выбранная вью-модель
+
+        private ViewModel _currentViewModel;
+
+        public ViewModel CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => Set(ref _currentViewModel, value);
+        }
+
+        #endregion
+
+        #region Список доступных вью-моделей
+
+        private List<ViewModel> _viewModelsList;
+        public List<ViewModel> ViewModelsList
+        {
+            get => _viewModelsList;
+            set => Set(ref _viewModelsList, value);
+        }
+
+        #endregion
+
+        #region Команды
+
+        public ICommand SelectViewModelCommand { get; set; }
+
+        private void OnSelectViewModelCommandExecute(object parameter)
+        {
+            ViewModel selectedViewModel = ViewModelsList.FirstOrDefault(vm => nameof(vm) == parameter.ToString());
+            CurrentViewModel = selectedViewModel;
+        }
+
+        private bool CanSelectViewModelCommandExecuted(object parameter) => true;
+
+        #endregion
+
+
+        public MainWindowViewModel()
+        {
+            SelectViewModelCommand = new RelayCommand(OnSelectViewModelCommandExecute, CanSelectViewModelCommandExecuted);
+        }
+
+    }
+}
