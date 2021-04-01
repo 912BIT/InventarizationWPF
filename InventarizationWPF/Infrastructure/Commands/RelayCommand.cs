9d2d61c4f8b8ef6970586466836533096b594908
912bit <freaks338@gmail.com>
2021-04-01 20:25:36 +0700

    Базовый класс для всех команд

---
+++ InventarizationWPF/Infrastructure/Commands/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InventarizationWPF.Infrastructure.Commands
+{
+    internal class RelayCommand : Command
+    {
+        /// <summary>
+        /// Ссылка на метод, который необходимо выполнить.
+        /// </summary>
+        private readonly Action<object> _execute;
+
+        /// <summary>
+        /// Ссылка на метод, который проверяет возможно ли выполнить команду.
+        /// </summary>
+        private readonly Func<object, bool> _canExecute;
+
+        /// <summary>
+        /// Проверяет возможно ли выполнить команду.
+        /// </summary>
+        /// <param name="parameter">параметр команды</param>
+        /// <returns></returns>
+        public override bool CanExecute(object parameter) => _canExecute == null ? true : _canExecute.Invoke(parameter);
+
+        /// <summary>
+        /// Выполняет команду.
+        /// </summary>
+        /// <param name="parameter">Параметр команды.</param>
+        /// <returns>True если команду можно выполнить, иначе False.</returns>
+        public override void Execute(object parameter) => _execute?.Invoke(parameter);
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+    }
+}
