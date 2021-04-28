2c73e2ff3220888d568c732b0c2b3c6df3450788
alextnull <alexander.t.null@outlook.com>
2021-04-28 13:01:13 +0700

    Модель оборудования

---
+++ InventarizationWPF/Models/Equipment.cs
@@ -0,0 +1,20 @@
+﻿namespace InventarizationWPF.Models
+{
+    internal class Equipment
+    {
+        /// <summary>Id оборудования</summary>
+        public int Id { get; set; }
+
+        /// <summary>Наименование оборудования</summary>
+        public string Name { get; set; }
+
+        /// <summary>Количество оборудования</summary>
+        public int Count { get; set; }
+
+        /// <summary>Цена оборудования</summary>
+        public int Price { get; set; }
+
+        /// <summary>Сумма оборудования</summary>
+        public int Sum { get; set; }
+    }
+}
