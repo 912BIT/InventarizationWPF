8a8d4e905425ff5da536c8a3f688773a756a57b9
alextnull <alexander.t.null@outlook.com>
2021-04-28 13:09:30 +0700

    Контекст данных для работы с БД

---
+++ InventarizationWPF/Data/InventarizationContext.cs
@@ -0,0 +1,18 @@
+﻿using InventarizationWPF.Models;
+using System.Data.Entity;
+
+namespace InventarizationWPF.Data
+{
+    /// <summary>Контекст данных для доступа к БД</summary>
+    internal class InventarizationContext : DbContext
+    {
+        /// <summary>Инициализация контекста данных БД</summary>
+        public InventarizationContext() : base(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=inventarization;Integrated Security=True;")
+        {
+
+        }
+
+        /// <summary>Список оборудования</summary>
+        public DbSet<Equipment> Equipment { get; set; }
+    }
+}
